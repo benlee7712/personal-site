0f6abebc911ab2b64611e6abd0689bbb577742b6
Ben Lee <benlee1151@gmail.com>
2022-04-30 22:50:39 +0100

    Update API config

--- api/src/main.rs
+++ api/src/main.rs
@@ -4,16 +4,16 @@ use std::path::PathBuf;
 
 /// https://actix.rs/docs/static-files/
 async fn index(_req: HttpRequest) -> Result<NamedFile> {
-    let path: PathBuf = "./files/index.html".parse().unwrap();
+    let path: PathBuf = "./src/dist/index.html".parse().unwrap();
     Ok(NamedFile::open(path)?)
 }
 
-#[actix_rt::main]
+#[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     use actix_web::{web, App, HttpServer};
 
     HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind("127.0.0.1:8088")?
+        .bind("127.0.0.1:8080")?
         .run()
         .await
 }
