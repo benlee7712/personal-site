38c23944a3866231216ef06c915fc100502a3c8e
Ben Lee <benlee1151@gmail.com>
2022-07-23 18:39:22 +0100

    Add photography page

---
+++ frontend/src/views/Photography.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="photo">
+    <h1>This is a page with my photos</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .photo {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
