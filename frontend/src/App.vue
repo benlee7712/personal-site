024ad345ff96579e0b6e8fd312d12455d63bac58
Ben Lee <benlee1151@gmail.com>
2022-07-24 22:37:42 +0100

    Drop header bg

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header class="mt-0 fixed w-full z-10 top-0 py-8 bg-white/40">
+  <header class="mt-0 fixed w-full z-10 top-0 py-8">
     <div class="flex-wrap flex mx-auto">
       <RouterLink to="/" class="flex w-1/2 justify-start content-start">
         <h1 class="flex flex-none justify-start pl-8 sm:pl-16 font-semibold font-rubik text-6xl md:text-8xl name-title">B<h1 class="font-semibold font-rubik text-6xl md:text-8xl hidden-title hidden lg:inline">en Lee</h1></h1>
