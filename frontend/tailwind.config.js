11f222013c50eb51584031711cc541f928fecced
Ben Lee <benlee1151@gmail.com>
2022-07-24 12:47:26 +0100

    Neaten out highlights with border-radius

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,18 @@ module.exports = {
       'humane': ['Humane'],
       'open': ['Open Sans', 'sans-serif'],
     },
+    fontSize: {
+      'xs': ['0.75rem', '1rem'],
+      'sm': ['0.875rem', '1.25rem'],
+      'base': ['1rem', '1.5rem'],
+      'lg': ['1.125rem', '1.75rem'],
+      'xl': ['1.25rem', '1.75rem'],
+      '2xl': ['1.5rem', '2rem'],
+      '3xl': ['1.875rem', '2.25rem'],
+      '4xl': ['2.25rem', '2.5rem'],
+      '5xl': ['3rem', '3.25rem'],
+      '6xl': ['3.75rem', '1.25'],
+    },
     colors: {
       coral: '#FF6F5F',
       lime: '#5FFF6F',
