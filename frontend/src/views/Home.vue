44791ba0fe0ca453e5c239859eed10c9e535eb35
Ben Lee <benlee1151@gmail.com>
2022-07-24 23:39:10 +0100

    Fix arrow sits below page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,11 +3,11 @@
         <full-page>
             <div class="section section-1 h-screen">
                 <div class="w-full flex fade-slide-in-top">
-                    <h2 class="mt-[50vh] translate-y-[-50%] py-1 mx-[10%] sm:mx-[10%] font-open text-3xl sm:text-4xl lg:text-5xl 2xl:text-6xl text-slate text-left align-middle corner-borders-coral leading-normal">
+                    <h2 class="py-1 mx-[10%] sm:mx-[10%] font-open text-3xl sm:text-4xl lg:text-5xl 2xl:text-6xl text-slate text-left align-middle corner-borders-coral leading-normal">
                         Hi. I'm <h2 class="inline bg-coral rounded px-[2px]">Ben Lee,</h2> a Software Developer at <a href="https://kayenta.io" class="text-lapis link-underline">Kayenta.</a> Nice to meet you.
                     </h2>
                 </div>
-                <div class="w-full mt-auto h-[10vh] bottom-0">
+                <div class="w-full mt-auto h-[10vh] bottom-0 absolute">
                     <img class="move-up-down m-auto w-[20vw] h-[5vh]" src="../assets/icons/down-arrow.svg">
                 </div>
             </div>
