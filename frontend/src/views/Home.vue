f1c50589228a6965313ce4925a444d1c09bbdb4d
Ben Lee <benlee1151@gmail.com>
2022-07-24 22:38:02 +0100

    Add moving arrow

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,7 +7,9 @@
                         Hi. I'm <h2 class="inline bg-coral rounded px-[2px]">Ben Lee,</h2> a Software Developer at <a href="https://kayenta.io" class="text-lapis link-underline">Kayenta.</a> Nice to meet you.
                     </h2>
                 </div>
-                <img class="bottom-0 flex" src="../assets/icons/down-arrow.svg">
+                <div class="w-full mt-auto h-[10vh] bottom-0">
+                    <img class="move-up-down m-auto w-[20vw] h-[5vh]" src="../assets/icons/down-arrow.svg">
+                </div>
             </div>
             <div class="section bg-slate">
                 <div class="w-full h-[100vh] flex justify-center items-center fade-slide-in-top">
