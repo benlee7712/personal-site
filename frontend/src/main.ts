ea6ae6f983a982693f0e059a1f067939ee96c8b7
Ben Lee <benlee1151@gmail.com>
2022-07-24 12:46:59 +0100

    Add vue full page

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+
 import router from "./router";
+import 'vue-fullpage.js/dist/style.css'
+import VueFullPage from 'vue-fullpage.js'
 import './index.css'
 
 const app = createApp(App);
 
 app.use(router);
+app.use(VueFullPage);
 
 app.mount("#app");
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,10 +1,14 @@
 <template>
     <body>
-        <div class="w-full h-[100vh] flex justify-center items-center">
-            <h2 class="mx-[15%] sm:mx-[25%] translate-y-[-25%] font-open text-4xl sm:text-6xl text-slate-600 text-left align-middle short-underline-coral">
-                Hi. I'm <h2 class="inline highlight-coral">Ben Lee,</h2> a Software Developer at Kayenta. Nice to meet you.
-            </h2>
-        </div>
-        <img class="bottom-0 flex" src="../assets/icons/down-arrow.svg">
+        <full-page>
+            <div class="section">
+                <div class="w-full h-[100vh] flex justify-center items-center">
+                    <h2 class="py-1 mx-[15%] sm:mx-[25%] translate-y-[-25%] font-open text-4xl sm:text-6xl text-slate-600 text-left align-middle short-underline-coral leading-normal">
+                        Hi. I'm <h2 class="inline bg-coral rounded">Ben Lee,</h2> a Software Developer at Kayenta. Nice to meet you.
+                    </h2>
+                </div>
+                <img class="bottom-0 flex" src="../assets/icons/down-arrow.svg">
+            </div>
+        </full-page>
     </body>
 </template>
