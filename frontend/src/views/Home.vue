c08d913893e2099513ce8922e3393537d78c18c6
Ben Lee <benlee1151@gmail.com>
2022-07-24 19:13:03 +0100

    Add fade slide in on load animation

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,9 +2,9 @@
     <body>
         <full-page>
             <div class="section">
-                <div class="w-full h-[100vh] flex justify-center items-center">
                     <h2 class="py-1 mx-[15%] sm:mx-[25%] translate-y-[-25%] font-open text-4xl sm:text-6xl text-slate text-left align-middle short-underline-coral leading-normal">
                         Hi. I'm <h2 class="inline bg-coral rounded">Ben Lee,</h2> a Software Developer at <a href="https://kayenta.io" class="text-lapis link-underline">Kayenta.</a> Nice to meet you.
+                <div class="w-full h-[100vh] flex justify-center items-center fade-slide-in-top">
                     </h2>
                 </div>
                 <img class="bottom-0 flex" src="../assets/icons/down-arrow.svg">
