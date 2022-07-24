78dc15c4aeb66139ee948f7b524c1fa2d5872a65
Ben Lee <benlee1151@gmail.com>
2022-07-24 02:34:55 +0100

    Update Photography.vue

--- frontend/src/views/Photography.vue
+++ frontend/src/views/Photography.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="photo">
-    <h1 class="w-full text-center text-4xl sm:text-6xl font-open">Coming soon...</h1>
+  <div class="h-full">
+    <h1 class="w-full my-[40vh] text-center text-4xl sm:text-6xl font-open">Coming soon...</h1>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .photo {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
