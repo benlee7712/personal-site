b26b77df053ef3a7754407066efe6996ac03f3a6
Ben Lee <benlee1151@gmail.com>
2022-07-24 01:36:50 +0100

    Update routes.ts

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,6 +1,14 @@
+import Home from "../views/Home.vue"
 import Photography from "../views/Photography.vue";
 
 export default [
+    {
+        path: "/",
+        component: Home,
+        meta: {
+          title: "Home",
+        },
+    },
     {
         path: "/photography",
         component: Photography,
