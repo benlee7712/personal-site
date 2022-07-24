21ded867bc26e9e93e8fb52dcc10d616ca73921d
Ben Lee <benlee1151@gmail.com>
2022-07-24 23:32:31 +0100

    Change short underline to corner borders

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,7 +3,7 @@
         <full-page>
             <div class="section section-1 h-screen">
                 <div class="w-full flex fade-slide-in-top">
-                    <h2 class="mt-[50vh] translate-y-[-50%] py-1 mx-[15%] sm:mx-[25%] font-open text-3xl sm:text-4xl lg:text-5xl 2xl:text-6xl text-slate text-left align-middle short-underline-coral leading-normal">
+                    <h2 class="mt-[50vh] translate-y-[-50%] py-1 mx-[10%] sm:mx-[10%] font-open text-3xl sm:text-4xl lg:text-5xl 2xl:text-6xl text-slate text-left align-middle corner-borders-coral leading-normal">
                         Hi. I'm <h2 class="inline bg-coral rounded px-[2px]">Ben Lee,</h2> a Software Developer at <a href="https://kayenta.io" class="text-lapis link-underline">Kayenta.</a> Nice to meet you.
                     </h2>
                 </div>
