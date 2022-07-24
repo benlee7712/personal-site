1578b761e107f89e4f7c2c5c5074ea0843bc7cfb
Ben Lee <benlee1151@gmail.com>
2022-07-24 22:38:21 +0100

    Fix styling to cope with svg

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
     <body>
         <full-page>
-            <div class="section">
-                <div class="w-full h-[100vh] flex justify-center items-center fade-slide-in-top">
-                    <h2 class="py-1 mx-[15%] sm:mx-[25%] translate-y-[-25%] font-open text-3xl sm:text-6xl text-slate text-left align-middle short-underline-coral leading-normal">
+            <div class="section section-1 h-screen">
+                <div class="w-full flex fade-slide-in-top">
+                    <h2 class="mt-[50vh] translate-y-[-50%] py-1 mx-[15%] sm:mx-[25%] font-open text-3xl sm:text-4xl lg:text-5xl 2xl:text-6xl text-slate text-left align-middle short-underline-coral leading-normal">
                         Hi. I'm <h2 class="inline bg-coral rounded px-[2px]">Ben Lee,</h2> a Software Developer at <a href="https://kayenta.io" class="text-lapis link-underline">Kayenta.</a> Nice to meet you.
                     </h2>
                 </div>
