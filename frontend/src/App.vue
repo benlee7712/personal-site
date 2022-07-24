16dc68770991dd433c5d2cf947ad6758da25185b
Ben Lee <benlee1151@gmail.com>
2022-07-24 01:37:06 +0100

    Fix header styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,20 +6,22 @@ import { RouterLink, RouterView } from "vue-router";
   <header class="mt-0 fixed w-full z-10 top-0 py-8">
     <div class="flex-wrap flex mx-auto">
       <div class="flex w-1/2 justify-start content-start">
-        <h1 class="flex flex-none justify-start pl-16 text-slate-500 font-semibold font-rubik text-6xl md:text-8xl name-title">B<h1 class="text-slate-500 font-semibold font-rubik text-6xl md:text-8xl hidden-title inline">EN LEE</h1></h1>
+        <h1 class="flex flex-none justify-start pl-8 sm:pl-16 text-slate-600 font-semibold font-rubik text-6xl md:text-8xl name-title">B<h1 class="text-slate-600 font-semibold font-rubik text-6xl md:text-8xl hidden-title hidden lg:inline">en Lee</h1></h1>
       </div>
-      <div class="flex w-1/2 content-center justify-end pr-2 md:pr-8 lg:pr-16">
+      <div class="flex w-1/2 content-center justify-end pr-2 md:pr-8 lg:pr-16 z-15">
         <nav class="list-reset flex justify-between flex-none items-center">
-          <div class="px-5 md:px-8">
-            <RouterLink class="text-base md:text-lg lg:text-2xl font-open link-underline" to="/">Home</RouterLink>
+          <div class="px-1 sm:px-3 md:px-6 border-r-[1px] border-black">
+            <RouterLink class="px-[2px] md:px-2 pb-[2px] text-base sm:text-lg md:text-xl lg:text-2xl font-open link-underline" to="/">Home</RouterLink>
           </div>
-          <div class="px-5 md:px-8">
-            <RouterLink class="text-base md:text-lg lg:text-2xl font-open link-underline" to="/photography">Photography</RouterLink>
+          <div class="px-1 sm:px-3 md:px-6 border-r-[1px] border-black">
+            <RouterLink class="px-[2px] md:px-2 pb-[2px] text-base sm:text-lg md:text-xl lg:text-2xl font-open link-underline" to="/photography">Photography</RouterLink>
+          </div>
+          <div class="px-1 sm:px-3 md:px-6">
+            <RouterLink class="px-[2px] md:px-2 pb-[2px] text-base sm:text-lg md:text-xl lg:text-2xl font-open link-underline" to="/contact-me">Contact Me</RouterLink>
           </div>
         </nav>
       </div>
     </div>
   </header>
-
-  <RouterView />
+  <RouterView class="selection:bg-coral"/>
 </template>
