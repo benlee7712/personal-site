04b9ffd70191d7173722a7737fb5e3ed5566d796
Ben Lee <benlee1151@gmail.com>
2022-07-25 01:52:46 +0100

    Add horizontal scroll logos

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -11,11 +11,27 @@
                     <img class="move-up-down m-auto w-[20vw] h-[5vh]" src="../assets/icons/down-arrow.svg">
                 </div>
             </div>
-            <div class="section section-2 bg-slate">
-                <div class="w-full h-[100vh] flex justify-center items-center">
+            <div class="section section-2 h-screen bg-slate">
+                <div class="w-full flex justify-center items-center">
                     <h2 class="py-1 mx-[15%] sm:mx-[25%] translate-y-[-25%] font-open text-3xl sm:text-4xl lg:text-5xl 2xl:text-6xl text-white text-left align-middle leading-normal">
                         I <h2 class="inline bg-coral rounded px-[2px]">love</h2> working with all different kinds of technology... and learning new ones.
                     </h2>
+                </div>
+                <div class="scrolling-icons-wheel w-full h-[20vh]">
+                    <div class="scrolling-icons-slide">
+                        <img class="icon" src="@/assets/logos/skills/airflow.png">
+                        <img class="icon" src="@/assets/logos/skills/docker.svg">
+                        <img class="icon" src="@/assets/logos/skills/django.svg">
+                        <img class="icon" src="@/assets/logos/skills/github.svg">
+                        <img class="icon" src="@/assets/logos/skills/fastapi.svg">
+                    </div>
+                    <div class="scrolling-icons-slide delay">
+                        <img class="icon" src="@/assets/logos/skills/postgresql.svg">
+                        <img class="icon" src="@/assets/logos/skills/AWS.svg">
+                        <img class="icon" src="@/assets/logos/skills/python.svg">
+                        <img class="icon" src="@/assets/logos/skills/rust.svg">
+                        <img class="icon" src="@/assets/logos/skills/azure.svg">
+                    </div>
                 </div>
             </div>
         </full-page>
