bcb90603427c72cd2e54c00be14447269050ddc7
Ben Lee <benlee1151@gmail.com>
2022-12-05 01:34:07 +0000

    update colours

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,7 +4,7 @@
             <div class="section section-1 h-screen">
                 <div class="w-full flex fade-slide-in-top">
                     <h2 class="py-1 mx-[10%] sm:mx-[10%] font-open text-3xl sm:text-4xl lg:text-5xl 2xl:text-6xl text-slate text-left align-middle corner-borders-coral leading-normal">
-                        Hi. I'm <h2 class="inline bg-coral rounded px-[2px]">Ben Lee,</h2> a Software Developer at <a href="https://kayenta.io" class="text-lapis link-underline">Kayenta.</a> Nice to meet you.
+                        Hi. I'm <h2 class="inline bg-dusty_red rounded px-[2px]">Ben Lee,</h2> a Software Developer at <a href="https://kayenta.io" class="text-lapis link-underline">Kayenta.</a> Nice to meet you.
                     </h2>
                 </div>
                 <div class="w-full mt-auto h-[10vh] bottom-0 absolute">
