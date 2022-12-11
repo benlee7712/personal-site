3aff5dc7c84dc4bfe5b6a72fd4e18e127e512470
Ben Lee <benlee1151@gmail.com>
2022-12-11 02:15:34 +0000

    Add next arrow

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,26 +2,28 @@
     <body>
         <Carousel id="project_titles" :items-to-show="1" :wrap-around="true" v-model="currentSlide">
             <Slide :key="0"><div class="carousel__item">
-                <h1 class="text-3xl sm:text-[2.5rem] md:text-6xl 2xl:text-7xl 2xl:mb-[-1rem] font-cormorant font-light w-fit">USA</h1>
+                <h1 class="text-3xl sm:text-[2.5rem] md:text-5xl 2xl:text-7xl 2xl:mb-[-1rem] font-cormorant font-light w-fit">USA</h1>
                 <h1 class="text-6xl sm:text-6xl md:text-7xl 2xl:text-9xl font-oswald float-left">CALIFORNIA</h1>
-                <p class="text-xs md:text-sm 2xl:text-lg font-outfit font-medium text-dusty_red float-left pt-[2.5rem] sm:pt-[2.8rem] md:pt-[3.7rem] 2xl:pt-[8.7rem] pl-1 md:pl-2">[2022]</p>
+                <p class="text-xs md:text-sm 2xl:text-lg font-outfit font-medium text-dusty_red float-left pt-[2.8rem] sm:pt-[2.8rem] md:pt-[3.7rem] 2xl:pt-[8.7rem] pl-1 md:pl-2">[2022]</p>
             </div></Slide>
             <Slide :key="2"><div class="carousel__item">
-                <h1 class="text-3xl sm:text-[2.5rem] md:text-6xl 2xl:text-7xl 2xl:mb-[-1rem] font-cormorant font-light w-fit">GREECE</h1>
+                <h1 class="text-3xl sm:text-[2.5rem] md:text-5xl 2xl:text-7xl 2xl:mb-[-1rem] font-cormorant font-light w-fit">GREECE</h1>
                 <h1 class="text-6xl sm:text-6xl md:text-7xl 2xl:text-9xl font-oswald float-left">CORFU</h1>
-                <p class="text-xs md:text-sm 2xl:text-lg font-outfit font-medium text-dusty_red float-left pt-[2.5rem] sm:pt-[2.8rem] md:pt-[3.7rem] 2xl:pt-[8.7rem] pl-1 md:pl-2">[2022]</p>
+                <p class="text-xs md:text-sm 2xl:text-lg font-outfit font-medium text-dusty_red float-left pt-[2.8rem] sm:pt-[2.8rem] md:pt-[3.7rem] 2xl:pt-[8.7rem] pl-1 md:pl-2">[2022]</p>
             </div></Slide>
             <Slide :key="2"><div class="carousel__item">
-                <h1 class="text-3xl sm:text-[2.5rem] md:text-6xl 2xl:text-7xl 2xl:mb-[-1rem] font-cormorant font-light w-fit">GREECE</h1>
+                <h1 class="text-3xl sm:text-[2.5rem] md:text-5xl 2xl:text-7xl 2xl:mb-[-1rem] font-cormorant font-light w-fit">GREECE</h1>
                 <h1 class="text-6xl sm:text-6xl md:text-7xl 2xl:text-9xl font-oswald float-left">SANTORINI</h1>
-                <p class="text-xs md:text-sm 2xl:text-lg font-outfit font-medium text-dusty_red float-left pt-[2.5rem] sm:pt-[2.8rem] md:pt-[3.7rem] 2xl:pt-[8.7rem] pl-1 md:pl-2">[2022]</p>
+                <p class="text-xs md:text-sm 2xl:text-lg font-outfit font-medium text-dusty_red float-left pt-[2.8rem] sm:pt-[2.8rem] md:pt-[3.7rem] 2xl:pt-[8.7rem] pl-1 md:pl-2">[2022]</p>
             </div></Slide>
         </Carousel>
-
+        <div class="fixed link-highlight right-[5vw] bottom-[57.5vh] sm:bottom-[60vh] cursor-pointer" @click="slideTo(currentSlide + 1)">
+            <img class="h-8 md:h-10 2xl:h-16 px-2 float-right" src="@/assets/icons/next-arrow.svg">
+            <h1 class="text-2xl md:text-4xl 2xl:text-6xl font-outfit font-medium float-right pl-2">ANOTHER</h1>
+        </div>
         <Carousel
             id="thumbnails"
-            :items-to-show="2.5"
-            :wrap-around="true"
+            :settings="thumbnailCarouselSettings"
             v-model="currentSlide"
             ref="carousel"
         >
@@ -53,6 +55,11 @@ export default defineComponent({
   },
   data: () => ({
     currentSlide: 0,
+    thumbnailCarouselSettings: {
+        itemsToShow: 2.25,
+        wrapAround: true,
+        snapAlign: 'center',
+    },
   }),
   methods: {
     slideTo(val: number) {
