6ed5f7c1f4bf999b44bce54e927ede9b5648b28d
Ben Lee <benlee1151@gmail.com>
2022-12-11 17:52:37 +0000

    Styling update for mobile

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,24 +2,24 @@
     <body>
         <Carousel id="project_titles" :items-to-show="1" :wrap-around="true" v-model="currentSlide">
             <Slide :key="0"><div class="carousel__item">
-                <h1 class="text-3xl sm:text-[2.5rem] md:text-5xl 2xl:text-7xl 2xl:mb-[-1rem] font-cormorant font-light w-fit">USA</h1>
-                <h1 class="text-6xl sm:text-6xl md:text-7xl 2xl:text-9xl font-oswald float-left">CALIFORNIA</h1>
-                <p class="text-xs md:text-sm 2xl:text-lg font-outfit font-medium text-dusty_red float-left pt-[2.8rem] sm:pt-[2.8rem] md:pt-[3.7rem] 2xl:pt-[8.7rem] pl-1 md:pl-2">[2022]</p>
+                <h1 class="text-3xl sm:text-[2.5rem] md:text-4xl lg:text-5xl 2xl:text-7xl 2xl:mb-[-1rem] font-cormorant font-light w-fit">USA</h1>
+                <h1 class="text-6xl sm:text-6xl md:text-6xl lg:text-7xl 2xl:text-9xl font-oswald float-left">CALIFORNIA</h1>
+                <p class="text-xs md:text-sm 2xl:text-lg font-outfit font-medium text-dusty_red float-left pt-[2.8rem] sm:pt-[2.8rem] md:pt-[2.65rem] lg:pt-[3.7rem] 2xl:pt-[8.7rem] pl-1 md:pl-2">[2022]</p>
+            </div></Slide>
+            <Slide :key="1"><div class="carousel__item">
+                <h1 class="text-3xl sm:text-[2.5rem] md:text-4xl lg:text-5xl 2xl:text-7xl 2xl:mb-[-1rem] font-cormorant font-light w-fit">GREECE</h1>
+                <h1 class="text-6xl sm:text-6xl md:text-6xl lg:text-7xl 2xl:text-9xl font-oswald float-left">CORFU</h1>
+                <p class="text-xs md:text-sm 2xl:text-lg font-outfit font-medium text-dusty_red float-left pt-[2.8rem] sm:pt-[2.8rem] md:pt-[2.65rem] lg:pt-[3.7rem] 2xl:pt-[8.7rem] pl-1 md:pl-2">[2022]</p>
             </div></Slide>
             <Slide :key="2"><div class="carousel__item">
-                <h1 class="text-3xl sm:text-[2.5rem] md:text-5xl 2xl:text-7xl 2xl:mb-[-1rem] font-cormorant font-light w-fit">GREECE</h1>
-                <h1 class="text-6xl sm:text-6xl md:text-7xl 2xl:text-9xl font-oswald float-left">CORFU</h1>
-                <p class="text-xs md:text-sm 2xl:text-lg font-outfit font-medium text-dusty_red float-left pt-[2.8rem] sm:pt-[2.8rem] md:pt-[3.7rem] 2xl:pt-[8.7rem] pl-1 md:pl-2">[2022]</p>
-            </div></Slide>
-            <Slide :key="2"><div class="carousel__item">
-                <h1 class="text-3xl sm:text-[2.5rem] md:text-5xl 2xl:text-7xl 2xl:mb-[-1rem] font-cormorant font-light w-fit">GREECE</h1>
-                <h1 class="text-6xl sm:text-6xl md:text-7xl 2xl:text-9xl font-oswald float-left">SANTORINI</h1>
-                <p class="text-xs md:text-sm 2xl:text-lg font-outfit font-medium text-dusty_red float-left pt-[2.8rem] sm:pt-[2.8rem] md:pt-[3.7rem] 2xl:pt-[8.7rem] pl-1 md:pl-2">[2022]</p>
+                <h1 class="text-3xl sm:text-[2.5rem] md:text-4xl lg:text-5xl 2xl:text-7xl 2xl:mb-[-1rem] font-cormorant font-light w-fit">GREECE</h1>
+                <h1 class="text-6xl sm:text-6xl md:text-6xl lg:text-7xl 2xl:text-9xl font-oswald float-left">SANTORINI</h1>
+                <p class="text-xs md:text-sm 2xl:text-lg font-outfit font-medium text-dusty_red float-left pt-[2.8rem] sm:pt-[2.8rem] md:pt-[2.65rem] lg:pt-[3.7rem] 2xl:pt-[8.7rem] pl-1 md:pl-2">[2022]</p>
             </div></Slide>
         </Carousel>
-        <div class="next-arrow fixed link-highlight right-[5vw] bottom-[56vh] sm:bottom-[60vh] cursor-pointer" @click="slideTo(currentSlide + 1)">
-            <img class="h-8 md:h-10 2xl:h-16 px-2 float-right" src="@/assets/icons/next-arrow.svg">
-            <h1 class="text-2xl md:text-4xl 2xl:text-6xl font-outfit font-medium float-right pl-2">ANOTHER</h1>
+        <div class="next-arrow fixed link-highlight-mobile right-[5vw] bottom-[55vh] sm:bottom-[60vh] cursor-pointer" @click="slideTo(currentSlide + 1)">
+            <img class="h-8 md:h-8 lg:h-10 2xl:h-16 px-2 float-right" src="@/assets/icons/next-arrow.svg">
+            <h1 class="text-2xl md:text-3xl lg:text-4xl 2xl:text-6xl font-outfit font-medium float-right pl-2">ANOTHER</h1>
         </div>
         <Carousel
             id="thumbnails"
@@ -37,7 +37,7 @@
                 <img src="@/assets/images/_MG_6320_browsable.jpg">
             </div></Slide>
         </Carousel>
-        <div class="flex fixed bottom-0 sm:hidden w-full justify-center items-center h-[7.5vh]">
+        <div class="flex fixed bottom-0 sm:hidden w-full justify-center items-start h-[12.5vh] pt-[2vh]">
             <h1 class="text-dusty_red text-lg font-outfit">[ SEE PROJECT ]</h1>
         </div>
     </body>
@@ -62,9 +62,9 @@ export default defineComponent({
   data: () => ({
     currentSlide: 0,
     thumbnailCarouselSettings: {
-        itemsToShow: ((isDesktop.value) ? 2.25 : 1.5),
+        itemsToShow: ((isDesktop.value) ? 2.25 : 1.4),
         wrapAround: true,
-        snapAlign: ((isDesktop.value) ? 'center' : 'start'),
+        snapAlign: ((isDesktop.value) ? 'start' : 'start'),
     },
   }),
   methods: {
