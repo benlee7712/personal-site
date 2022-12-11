ccb319607eb311f340e648bbcb76dd0b9120b44f
Ben Lee <benlee1151@gmail.com>
2022-12-11 00:33:09 +0000

    Fix year indicator

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
       'lg': ['1.125rem', '1.75rem'],
       'xl': ['1.25rem', '1.75rem'],
       '2xl': ['1.5rem', '2rem'],
-      '3xl': ['1.75rem', '2rem'],
+      '3xl': ['2rem', '2rem'],
       '4xl': ['2.25rem', '2.5rem'],
       '5xl': ['3rem', '3.25rem'],
       '6xl': ['3.75rem', '4rem'],
