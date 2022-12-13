04311651fcf00fb476c91be461703600d317619e
Ben Lee <benlee1151@gmail.com>
2022-12-13 23:19:51 +0000

    Fix iOS no underline

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,14 +2,14 @@
     <body class="overflow-hidden">
         <Carousel id="project_titles" :items-to-show="1" :wrap-around="true" v-model="currentSlide">
             <Slide v-for="[key, project] of projectData.entries()" :key="key"><div class="carousel__item">
-                <h1 class="text-3xl sm:text-[2.5rem] md:text-4xl lg:text-5xl 2xl:text-7xl 2xl:mb-[-1rem] font-cormorant font-light italic w-fit">{{ project.country }}</h1>
-                <h1 class="text-6xl sm:text-6xl md:text-6xl lg:text-7xl 2xl:text-9xl font-oswald float-left">{{ project.location }}</h1>
-                <p class="text-xs md:text-sm 2xl:text-lg font-outfit font-medium text-dusty_red float-left pt-[2.8rem] sm:pt-[2.8rem] md:pt-[2.65rem] lg:pt-[3.7rem] 2xl:pt-[8.7rem] pl-1 md:pl-2">[{{ project.year }}]</p>
+                <h1 class="text-3xl md:text-3xl lg:text-5xl 2xl:text-7xl 2xl:mb-[-1rem] font-cormorant font-light italic w-fit">{{ project.country }}</h1>
+                <h1 class="text-6xl md:text-[4.25rem] lg:text-7xl 2xl:text-10xl font-oswald float-left">{{ project.location }}</h1>
+                <p class="text-xs md:text-sm 2xl:text-lg font-outfit font-medium text-dusty_red float-left pt-[2.8rem] sm:pt-[2.8rem] md:pt-[2.9rem] lg:pt-[3.7rem] 2xl:pt-[8.7rem] pl-1 md:pl-2">[{{ project.year }}]</p>
             </div></Slide>
         </Carousel>
-        <div class="next-arrow fixed right-[5vw] bottom-[55vh] sm:bottom-[55vh] cursor-pointer pb-1" @click="slideTo(currentSlide + 1)">
+        <div class="next-arrow fixed right-[5vw] bottom-[55vh] sm:bottom-[55vh] cursor-pointer" @click="slideTo(currentSlide + 1)">
             <img class="h-8 md:h-8 lg:h-10 2xl:h-16 px-2 float-right" src="@/assets/icons/next-arrow.svg">
-            <h1 class="text-2xl md:text-3xl lg:text-4xl 2xl:text-6xl font-outfit font-medium float-right pl-2 red-underline">ANOTHER</h1>
+            <h1 class="text-2xl md:text-3xl lg:text-4xl 2xl:text-6xl font-outfit font-medium float-right red-underline">ANOTHER</h1>
         </div>
         <Carousel
             id="thumbnails"
