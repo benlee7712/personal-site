80e841f22fd8e319e562b60039aa040c0ef33ec6
Ben Lee <benlee1151@gmail.com>
2022-12-18 22:26:53 +0000

    Use underline not highlight for links

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,14 +7,14 @@ import { RouterLink, RouterView } from "vue-router";
     <div class="flex-wrap flex mx-auto" style="border-bottom: 1px solid #979797;">
       <div class="flex w-1/2 content-start justify-start pl-[calc(5vw-8px)] pb-2 lg:pb-3 xl:pb-4 z-15">
         <nav class="list-reset flex justify-between flex-none items-end">
-          <div class="">
-            <RouterLink class="px-2 text-2xs sm:text-xs lg:text-sm xl:text-base 2xl:text-xl font-outfit link-highlight" to="/">PROJECTS</RouterLink>
+          <div>
+            <RouterLink class="mx-2 pb-0.5 text-2xs sm:text-xs lg:text-sm xl:text-base 2xl:text-xl font-outfit link-underline" to="/">PROJECTS</RouterLink>
           </div>
           <div class="px-[1.5rem] sm:px-[3rem] md:px-[4rem] xl:px-[8rem] 2xl:px-[12.5rem]">
-            <RouterLink class="px-2 text-2xs sm:text-xs lg:text-sm xl:text-base 2xl:text-xl font-outfit link-highlight" to="/about">ABOUT</RouterLink>
+            <RouterLink class="mx-2 pb-0.5 text-2xs sm:text-xs lg:text-sm xl:text-base 2xl:text-xl font-outfit link-underline" to="/about">ABOUT</RouterLink>
           </div>
-          <div class="">
-            <RouterLink class="px-2 text-2xs sm:text-xs lg:text-sm xl:text-base 2xl:text-xl font-outfit link-highlight" to="/contact">CONTACT</RouterLink>
+          <div>
+            <RouterLink class="mx-2 pb-0.5 text-2xs sm:text-xs lg:text-sm xl:text-base 2xl:text-xl font-outfit link-underline" to="/contact">CONTACT</RouterLink>
           </div>
         </nav>
       </div>
