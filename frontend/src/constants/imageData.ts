35fd7fe371da7ebdf263aed2b8001dcfcc9d939b
Ben Lee <benlee1151@gmail.com>
2022-12-19 00:50:08 +0000

    Update imageData.ts

--- frontend/src/constants/imageData.ts
+++ frontend/src/constants/imageData.ts
@@ -7,6 +7,34 @@ const imageData = [
         "isThumbnail": true
     },
     {
+        "location": "CALIFORNIA",
+        "country": "USA",
+        "year": "2022",
+        "thumbnailImage": "_MG_7407_browsable.jpg",
+        "isThumbnail": false
+    },
+    {
+        "location": "CALIFORNIA",
+        "country": "USA",
+        "year": "2022",
+        "thumbnailImage": "_MG_7571_browsable.jpg",
+        "isThumbnail": false
+    },
+    {
+        "location": "CALIFORNIA",
+        "country": "USA",
+        "year": "2022",
+        "thumbnailImage": "_MG_7715_browsable.jpg",
+        "isThumbnail": false
+    },
+    {
+        "location": "CALIFORNIA",
+        "country": "USA",
+        "year": "2022",
+        "thumbnailImage": "_MG_7741_browsable.jpg",
+        "isThumbnail": false
+    },
+    {
         "location": "CORFU",
         "country": "GREECE",
         "year": "2022",
@@ -14,12 +42,40 @@ const imageData = [
         "isThumbnail": true
     },
     {
+        "location": "CORFU",
+        "country": "GREECE",
+        "year": "2022",
+        "thumbnailImage": "_MG_7246_browsable.jpg",
+        "isThumbnail": false
+    },
+    {
         "location": "SANTORINI",
         "country": "GREECE",
         "year": "2022",
         "thumbnailImage": "_MG_6320_browsable.jpg",
         "isThumbnail": true
-    }
+    },
+    {
+        "location": "SANTORINI",
+        "country": "GREECE",
+        "year": "2022",
+        "thumbnailImage": "_MG_6402_browsable.jpg",
+        "isThumbnail": false
+    },
+    {
+        "location": "SANTORINI",
+        "country": "GREECE",
+        "year": "2022",
+        "thumbnailImage": "_MG_6474_browsable.jpg",
+        "isThumbnail": false
+    },
+    {
+        "location": "SANTORINI",
+        "country": "GREECE",
+        "year": "2022",
+        "thumbnailImage": "_MG_6478_browsable.jpg",
+        "isThumbnail": false
+    },
 ]
 
 export default imageData
