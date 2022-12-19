4cb1ce68cfe3cf7ab9c547882bad461f18aa9c88
Ben Lee <benlee1151@gmail.com>
2022-12-19 00:54:56 +0000

    Rename thumbnailImage -> imagePath

--- frontend/src/constants/imageData.ts
+++ frontend/src/constants/imageData.ts
@@ -3,77 +3,77 @@ const imageData = [
         "location": "CALIFORNIA",
         "country": "USA",
         "year": "2022",
-        "thumbnailImage": "_MG_7395_browsable.jpg",
+        "imagePath": "_MG_7395_browsable.jpg",
         "isThumbnail": true
     },
     {
         "location": "CALIFORNIA",
         "country": "USA",
         "year": "2022",
-        "thumbnailImage": "_MG_7407_browsable.jpg",
+        "imagePath": "_MG_7407_browsable.jpg",
         "isThumbnail": false
     },
     {
         "location": "CALIFORNIA",
         "country": "USA",
         "year": "2022",
-        "thumbnailImage": "_MG_7571_browsable.jpg",
+        "imagePath": "_MG_7571_browsable.jpg",
         "isThumbnail": false
     },
     {
         "location": "CALIFORNIA",
         "country": "USA",
         "year": "2022",
-        "thumbnailImage": "_MG_7715_browsable.jpg",
+        "imagePath": "_MG_7715_browsable.jpg",
         "isThumbnail": false
     },
     {
         "location": "CALIFORNIA",
         "country": "USA",
         "year": "2022",
-        "thumbnailImage": "_MG_7741_browsable.jpg",
+        "imagePath": "_MG_7741_browsable.jpg",
         "isThumbnail": false
     },
     {
         "location": "CORFU",
         "country": "GREECE",
         "year": "2022",
-        "thumbnailImage": "_MG_7015_browsable.jpg",
+        "imagePath": "_MG_7015_browsable.jpg",
         "isThumbnail": true
     },
     {
         "location": "CORFU",
         "country": "GREECE",
         "year": "2022",
-        "thumbnailImage": "_MG_7246_browsable.jpg",
+        "imagePath": "_MG_7246_browsable.jpg",
         "isThumbnail": false
     },
     {
         "location": "SANTORINI",
         "country": "GREECE",
         "year": "2022",
-        "thumbnailImage": "_MG_6320_browsable.jpg",
+        "imagePath": "_MG_6320_browsable.jpg",
         "isThumbnail": true
     },
     {
         "location": "SANTORINI",
         "country": "GREECE",
         "year": "2022",
-        "thumbnailImage": "_MG_6402_browsable.jpg",
+        "imagePath": "_MG_6402_browsable.jpg",
         "isThumbnail": false
     },
     {
         "location": "SANTORINI",
         "country": "GREECE",
         "year": "2022",
-        "thumbnailImage": "_MG_6474_browsable.jpg",
+        "imagePath": "_MG_6474_browsable.jpg",
         "isThumbnail": false
     },
     {
         "location": "SANTORINI",
         "country": "GREECE",
         "year": "2022",
-        "thumbnailImage": "_MG_6478_browsable.jpg",
+        "imagePath": "_MG_6478_browsable.jpg",
         "isThumbnail": false
     },
 ]
