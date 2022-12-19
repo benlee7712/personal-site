a051b95a1140cc3bb1cec263c96b898fd4e039dc
Ben Lee <benlee1151@gmail.com>
2022-12-19 05:12:56 +0000

    Use fullpage js for projects

--- frontend/src/views/Project.vue
+++ frontend/src/views/Project.vue
@@ -1,15 +1,20 @@
 <template>
-    <body class="overflow-hidden">
-        <div v-for="[key, image] in imageData.filter(image => image.location === title).entries()" :key="key">
-            {{ image.location }}
-        </div>
-    </body>
+    <full-page :options="options">
+        <section v-for="[key, image] in imageData.filter(image => image.location === title).entries()" :key="key" class="w-full h-screen px-[5vw] flex justify-center items-center section">
+            <div>
+                <img :src="'images/' + image.imagePath" class="max-w-full max-h-[50vh]">
+            </div>
+        </section>
+    </full-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 import imageData from '../constants/imageData'
+
+
+const FULLPAGE_LICENSE = process.env.FULLPAGE_LICENSE
 
 export default defineComponent({
   name: 'Project',
@@ -20,6 +25,9 @@ export default defineComponent({
   },
   data: () => ({
     imageData: imageData,
+    options: {
+        licenseKey: FULLPAGE_LICENSE,
+    }
   }),
-})
+});
 </script>
