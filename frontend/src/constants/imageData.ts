a0b77e334b9adaba9c911dd598d914e964935a5f
Ben Lee <benlee1151@gmail.com>
2023-02-26 23:22:39 +0000

    Move fiji data to start of array

--- frontend/src/constants/imageData.ts
+++ frontend/src/constants/imageData.ts
@@ -1,5 +1,68 @@
 const imageData = [
     {
+        "location": "WAYALAILAI",
+        "country": "FIJI",
+        "year": "2023",
+        "imagePath": "_MG_7890_browsable.jpg",
+        "isThumbnail": false
+    },
+    {
+        "location": "WAYALAILAI",
+        "country": "FIJI",
+        "year": "2023",
+        "imagePath": "_MG_7955_browsable.jpg",
+        "isThumbnail": false
+    },
+    {
+        "location": "WAYALAILAI",
+        "country": "FIJI",
+        "year": "2023",
+        "imagePath": "_MG_7956_browsable.jpg",
+        "isThumbnail": true
+    },
+    {
+        "location": "WAYALAILAI",
+        "country": "FIJI",
+        "year": "2023",
+        "imagePath": "_MG_7973_browsable.jpg",
+        "isThumbnail": false
+    },
+    {
+        "location": "WAYALAILAI",
+        "country": "FIJI",
+        "year": "2023",
+        "imagePath": "_MG_7986_browsable.jpg",
+        "isThumbnail": false
+    },
+    {
+        "location": "WAYALAILAI",
+        "country": "FIJI",
+        "year": "2023",
+        "imagePath": "_MG_8018_browsable.jpg",
+        "isThumbnail": false
+    },
+    {
+        "location": "WAYALAILAI",
+        "country": "FIJI",
+        "year": "2023",
+        "imagePath": "_MG_8083_browsable.jpg",
+        "isThumbnail": false
+    },
+    {
+        "location": "WAYALAILAI",
+        "country": "FIJI",
+        "year": "2023",
+        "imagePath": "_MG_8093_browsable.jpg",
+        "isThumbnail": false
+    },
+    {
+        "location": "WAYALAILAI",
+        "country": "FIJI",
+        "year": "2023",
+        "imagePath": "_MG_8226_browsable.jpg",
+        "isThumbnail": false
+    },
+    {
         "location": "CALIFORNIA",
         "country": "USA",
         "year": "2022",
@@ -74,69 +137,6 @@ const imageData = [
         "country": "GREECE",
         "year": "2022",
         "imagePath": "_MG_6478_browsable.jpg",
-        "isThumbnail": false
-    },
-    {
-        "location": "WAYALAILAI",
-        "country": "FIJI",
-        "year": "2023",
-        "imagePath": "_MG_7890_browsable.jpg",
-        "isThumbnail": false
-    },
-    {
-        "location": "WAYALAILAI",
-        "country": "FIJI",
-        "year": "2023",
-        "imagePath": "_MG_7955_browsable.jpg",
-        "isThumbnail": false
-    },
-    {
-        "location": "WAYALAILAI",
-        "country": "FIJI",
-        "year": "2023",
-        "imagePath": "_MG_7956_browsable.jpg",
-        "isThumbnail": true
-    },
-    {
-        "location": "WAYALAILAI",
-        "country": "FIJI",
-        "year": "2023",
-        "imagePath": "_MG_7973_browsable.jpg",
-        "isThumbnail": false
-    },
-    {
-        "location": "WAYALAILAI",
-        "country": "FIJI",
-        "year": "2023",
-        "imagePath": "_MG_7986_browsable.jpg",
-        "isThumbnail": false
-    },
-    {
-        "location": "WAYALAILAI",
-        "country": "FIJI",
-        "year": "2023",
-        "imagePath": "_MG_8018_browsable.jpg",
-        "isThumbnail": false
-    },
-    {
-        "location": "WAYALAILAI",
-        "country": "FIJI",
-        "year": "2023",
-        "imagePath": "_MG_8083_browsable.jpg",
-        "isThumbnail": false
-    },
-    {
-        "location": "WAYALAILAI",
-        "country": "FIJI",
-        "year": "2023",
-        "imagePath": "_MG_8093_browsable.jpg",
-        "isThumbnail": false
-    },
-    {
-        "location": "WAYALAILAI",
-        "country": "FIJI",
-        "year": "2023",
-        "imagePath": "_MG_8226_browsable.jpg",
         "isThumbnail": false
     },
 ]
