d3d6fe1ffe837a63c9a5a3c03c792e2667132abf
Ben Lee <benlee1151@gmail.com>
2023-05-08 00:01:30 +0100

    Add vuetify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,9 +6,21 @@ import 'vue-fullpage.js/dist/style.css'
 import VueFullPage from 'vue-fullpage.js'
 import './index.css'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 const app = createApp(App);
 
 app.use(router);
 app.use(VueFullPage);
+app.use(vuetify);
 
 app.mount("#app");
