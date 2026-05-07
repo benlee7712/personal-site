7668233f6f1e964388c0f5b7554bfed2dcc08686
Ben Lee <benlee@192.168.1.163>
2026-05-07 19:26:39 +0100

    Update see more text

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -27,7 +27,7 @@
             </div></Slide>
         </Carousel>
         <RouterLink :to="{ path: '/project', query: { title: projectData[currentSlide % projectData.length].location }}" class="flex fixed bottom-0 w-full items-start sm:items-center h-[18vh] sm:h-[8vh] pt-[5vh] sm:pt-0 pl-[5vw]">
-            <h1 class="text-lg font-outfit red-border-box">{{ `VIEW ${projectData[currentSlide % projectData.length].location} PHOTOS`}}</h1>
+            <h1 class="text-lg font-outfit red-border-box">{{ `SEE MORE ${projectData[currentSlide % projectData.length].location} PHOTOS`}}</h1>
         </RouterLink>
     </body>
 </template>
